8c314044b31fa52e2412396e1cd1ded238cd64bf
Christelle Wehbe <libanon_cristelle@hotmail.com>
2015-02-24 10:17:07 +0100

    Update timereport_2.py
    Start/stop working save-button saving

--- website_project_timesheet_timer/timereport_2.py
+++ website_project_timesheet_timer/timereport_2.py
@@ -1,24 +1,3 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-# OpenERP, Open Source Management Solution, third party addon
-# Copyright (C) 2004-2015 Vertel AB (<http://vertel.se>).
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
 from openerp import models, fields, api, _
 from openerp.exceptions import except_orm, Warning, RedirectWarning
 from openerp import http
@@ -41,7 +20,8 @@ class project_timereport(http.Controller):
         cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
         if not user:
             return werkzeug.utils.redirect("/treport/%s/list" %uid,302)
-            
+
+
        
         ctx = {
             'user' : user,
@@ -50,29 +30,32 @@ class project_timereport(http.Controller):
             }
     
 
-        return request.render('timereport_2.project_timereport', ctx)
+        return request.render('website_project_timesheet_timer.project_timereport', ctx)
         
-    @http.route(['/treport/<model("res.users"):user>/<model("project.task"):task>'], type='http', auth="user", website=True)
-    def timereport_form(self, user=False,task=False, **post):
+    @http.route(['/treport/<model("res.users"):user>/<model("project.task"):task>/<int:start>'], type='http', auth="user", website=True)
+    def timereport_form(self, user=False, task=False, start=False, **post):
         cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
         if not user:
             return werkzeug.utils.redirect("/treport/%s/form" %uid,302)
-        
+        works=pool.get('project.task.work').search(cr,uid,['&',('task_id','=',task.id),('hours', '=', 0)])
         if request.httprequest.method == 'POST':
             _logger.warning(_("This is timereport post %s ") % (post))
-
             
-            task.start_stop_work(context, post.get('name'))
-            work.task_id.description(context, post.get('work')
+            if start == 1:
+                task.start_stop_work(context, post.get('name'))
+                return werkzeug.utils.redirect("/treport/%s" %user.id) 
+            
+            if len (works)!=0:
+                pool.get('project.task.work').browse(cr,uid,works[0]).name=post.get('name')
             return werkzeug.utils.redirect("/treport/%s" %user.id) 
-            
         
         
+
         ctx = {
-            'user' : user,
-            'task': task,  
-            'work': work,           
+            'user': user,
+            'task': task, 
+            'work': False if len (works)==0 else pool.get('project.task.work').browse(cr,uid,works[0]), 
             }
     
 
-        return request.render('timereport_2.project_timereport_form', ctx)
+        return request.render('website_project_timesheet_timer.project_timereport_form', ctx)
